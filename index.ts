a77ae30b803f56e219da380d2a1082906f1cfdc6
rezaaliabidi <syedrezaaliabidi@gmail.com>
2024-04-23 12:59:34 -0700

    student_management_system

--- index.ts
+++ index.ts
@@ -1,10 +1,4 @@
 #! usr/bin/env node
-//Students Management System Program
-//This project is a simple console based Student Management System. 
-//In this project you will be learning how to add new students, how to generate a 5 digit unique studentID for each student, 
-//how to enroll students in the given courses. Also, you will be implementing the following operations enroll, view balance, 
-//pay tuition fees, show status, etc. The status will show all the details of the student including name, id, courses enrolled 
-//and balance.This is one of the best projects to implement the Object Oriented Programming concepts.
 
 import inquirer from "inquirer";
 import chalk from "chalk";
